77aaafb52160fd7106a7ed872a2bc7f7eb391023
chandrakal <31609018+chandrakal@users.noreply.github.com>
2025-04-30 22:59:26 +0530

    Create or update from Migration

---
+++ angular-components/display/display.ts
@@ -0,0 +1,16 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-details',
+  templateUrl: './user-details.component.html',
+  styleUrls: ['./user-details.component.css']
+})
+export class UserDetailsComponent {
+  user = {
+    firstName: '',
+    lastName: '',
+    age: 0,
+    address: ''
+  };
+}
