ecf48274de01828745bc1cbedfde36cf3c38eea5
chandrakal <31609018+chandrakal@users.noreply.github.com>
2025-05-06 15:22:47 +0530

    Create or update from Migration

--- quarkus-api/src/main/java/UserService.java
+++ quarkus-api/src/main/java/UserService.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.Objects;
 
 @ApplicationScoped
 public class UserService {
@@ -12,7 +13,13 @@ public class UserService {
     private static final Logger logger = LogManager.getLogger(UserService.class);
 
     public void logUserDetails(UserBean userBean) {
-        logger.info("User Details: {}", userBean);
+        Objects.requireNonNull(userBean, "userBean is null");
+
+        logger.info("User Details:");
+        logger.info("First Name: {}", userBean.getFirstName());
+        logger.info("Last Name: {}", userBean.getLastName());
+        logger.info("Age: {}", userBean.getAge());
+        logger.info("Address: {}", userBean.getAddress());
     }
 }
 
